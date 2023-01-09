84a27d31ad57b32dc792fddfc4cfe400b56ed6dd
tomeryoel <96529795+tomeryoel@users.noreply.github.com>
2023-01-09 15:37:54 +0200

    Add files via upload

---
+++ Spring-Automation-SandBox1.6/src/test/java/com/Application/Spring/Automation/SandBox16/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Application.Spring.Automation.SandBox16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
